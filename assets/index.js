0c0ffecee81c0ac6d84cd1063fd1eba6ed24d015
psazama <patriciasazama@gmail.com>
2025-04-18 09:29:15 -0400

    Create index.js

---
+++ assets/index.js
@@ -0,0 +1,5 @@
+document.getElementById("root").innerHTML = `
+  <h1 style='text-align: center;'>WildWatt</h1>
+  <p style='text-align: center;'>Energizing Ecosystems</p>
+  <p style='max-width: 600px; margin: auto; text-align: center;'>This is a placeholder for the compiled React app. Replace this with actual React build output if needed.</p>
+`;
